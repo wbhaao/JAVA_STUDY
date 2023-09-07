608215964920c825e9bffc1b4371c0e1ecf9c7cd
wbhaao <82032508+wbhaao@users.noreply.github.com>
2023-09-07 00:14:28 +0000

    인터페이스 2

--- Main.java
+++ Main.java
@@ -1,9 +1,29 @@
 
 
-interface I {
-  public void z();
+public interface Calculatable {
+  public void setOprands(int first, int second, int third);
+  public int sum();
+  public int avg();
 }
 
-class A implements I {
-  public void z(){}
+class Calculator implements Calculatable {
+  int first, second, third;
+
+  public void setOprands(int first, int second, int third) {
+    this.first = first;
+    this.second = second;
+    this.third = third;
+  }
+  public int sum(){
+    return this.first + this.second + this.third;
+  }
+  public int avg() {
+    return (this.first + this.second + this.third)
+ / 2;  }
+}
+
+public class CalculatorConsumer {
+  public static void main(String[] args) {
+    Calculator c = new Calculator();
+  }
 }
